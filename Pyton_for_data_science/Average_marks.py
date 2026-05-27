068b731b2f2f6c82fae5ef024f6329ca7af70927
Birendra Paul <birendrapaul8022@gmail.com>
2026-05-27 22:16:36 +0530

    Add code for average marks of students

---
+++ Pyton_for_data_science/Average_marks.py
@@ -0,0 +1,23 @@
+# Find average marks of students
+students = [78,56,34,98,78,67,56,75,89,96]
+
+# sum add all marks
+# len counts total students
+avg = sum(students) / len(students)
+print(f"Avg marks of student is {avg}")
+
+
+
+# Problem -2:
+"""Create a Python program that:
+
+stores 5 expenses,
+calculates total expense,
+calculates average expense,
+prints both."""
+
+expenses = [100, 200, 300, 150, 250]
+total_expenses = sum(expenses)
+avg_expenses = total_expenses / len(expenses)
+print(f"Total Expenses is {total_expenses}")
+print(f"Average Expenses is {avg_expenses}")
